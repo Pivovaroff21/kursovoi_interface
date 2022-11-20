b13e198e7f89c72902a96942f284c72dfdb9cf40
Pyvovarav laptop <bogdan21122003@gmail.com>
2022-11-20 19:42:06 +0200

    tariff page modified

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@ const Operator = {
       tariffs: [
         {
           id: 1,
-          operatorId: 2,
+          operatorId: 1,
           name: "Сімейний",
           cost: 150,
         },
@@ -64,7 +64,7 @@ const Operator = {
         },
         {
           id: 3,
-          operatorId: 3,
+          operatorId: 1,
           name: "Средний",
           cost: 75,
         },
@@ -76,13 +76,13 @@ const Operator = {
         },
         {
           id: 5,
-          operatorId: 2,
+          operatorId: 1,
           name: "Інтернет на повну",
           cost: 320,
         },
         {
           id: 6,
-          operatorId: 3,
+          operatorId: 1,
           name: "ТОп за свої гроші",
           cost: 190,
         },
@@ -105,14 +105,15 @@ const Operator = {
     this.getOperator();
   },
   template: `
-      <h1 class="operator_name">{{operator.name}}</h1>
-        <div class="operator_cards_wrap">
-          <div  v-for="t in tariff">
-            <router-link :to="{name:'tariffId', params: {id: t.id}}" class="operator_link">
-                <div class="operator_card">
-                  <h2 class="tariff_name">{{t.name}}</h2>
-                  <p class="tariff_cost">{{t.cost}}</p>
-                </div>
+      <h1 class="operator_name tariff_operator_name">{{operator.name}}</h1>
+        <div class="tariff_cards_wrap">
+          <div  class="tariff_card" v-for="t in tariff">
+            <h2 class="tariff_name">{{t.name}}</h2>
+            <p class="tariff_cost">{{t.cost}} грн/міс</p>
+            <p class="tariff_desc"> Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. 
+            Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. </p>
+            <router-link :to="{name:'tariffId', params: {id: t.id}}" class="tariff_link">
+                Оплатити
             </router-link>
           </div>
       </div>`,
@@ -204,6 +205,88 @@ const PayPage = {
                 ВВедите что-то
                 <input type="text">
             </label>
+            <router-link to="/pay/card" class="tariff_link">
+                Оплатити
+            </router-link>
+        </form>
+      </div>
+       `,
+};
+
+const PayCard = {
+  data() {
+    return {
+      operators: [
+        {
+          id: 1,
+          name: "Vodafone",
+        },
+        {
+          id: 2,
+          name: "Kyivstar",
+        },
+        {
+          id: 3,
+          name: "Lifecell",
+        },
+      ],
+      tariffs: [
+        {
+          id: 1,
+          operatorId: 2,
+          name: "Сімейний",
+          cost: 150,
+        },
+        {
+          id: 2,
+          operatorId: 1,
+          name: "Для бомжей",
+          cost: 15,
+        },
+        {
+          id: 3,
+          operatorId: 3,
+          name: "Средний",
+          cost: 75,
+        },
+        {
+          id: 4,
+          operatorId: 1,
+          name: "Веселий",
+          cost: 228,
+        },
+        {
+          id: 5,
+          operatorId: 2,
+          name: "Інтернет на повну",
+          cost: 320,
+        },
+        {
+          id: 6,
+          operatorId: 3,
+          name: "ТОп за свої гроші",
+          cost: 190,
+        },
+      ],
+      operator: {},
+      tariff: {},
+    };
+  },
+  methods: {
+  },
+  created() {
+  },
+  template: `
+      <div class="wrapper">
+        <form action="">
+            <label for="">
+                ВВедите что-то
+                <input type="text">
+            </label>
+            <label for="">
+                ВВедите что-то
+                <input type="text">
+            </label>
             <button type="submit">submit</button>
         </form>
       </div>
@@ -223,6 +306,10 @@ const routes = [
     component: PayPage,
     name: "tariffId",
     props: true,
+  },
+  {
+    path: "/pay/card",
+    component: PayCard,
   },
 ];
 
